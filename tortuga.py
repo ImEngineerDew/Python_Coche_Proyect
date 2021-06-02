76c66668db02c83056af1bec5fdf27b16e7ea898
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-02 17:06:13 -0500

    Now I'm track some random files

--- tortuga.py
+++ tortuga.py
@@ -11,6 +11,7 @@ def make_square(dewin):
 	length = int(input("Writhe the square's size: "))
 	for i in range (4):
 		gire_lados(dewin,length)
+		
 
 def gire_lados(dewin,length):
 	dewin.forward(length)
---
+++ triangulo.py
@@ -0,0 +1,18 @@
+def triangulo():
+
+    lado1 = int(input("Ingrese el lado 1: "))
+    lado2 = int(input("Ingrese el lado 2: "))
+    lado3 = int(input("Ingrese el lado 3: "))    
+
+    if (lado1 < (lado2+lado3)):
+        print("Es un triángulo")
+    elif (lado2< (lado1+lado3)):
+        print("Es un triángulo")
+    elif (lado3 < (lado1+lado2)):
+        print("Es un triángulo")
+
+    if((lado1 == lado2) and (lado1 == lado3) and (lado2==lado3)):
+        print("Es un triángulo equilátero")
+
+if __name__ == '__main__':
+    triangulo()
